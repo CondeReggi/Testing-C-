da6c6f389f45aaeec2337b5541e6304589352ed6
CondeReggi <srtoxing@gmail.com>
2022-10-17 10:42:07 -0300

    Add project files.

---
+++ Ping/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pinging
+{
+
+    public class Program
+    {
+        public static async Task<string> Pinger(string url, int seconds)
+        {
+            try
+            {
+                using (var pinger = new Ping())
+                {
+                    var reply = await pinger.SendPingAsync(url, seconds * 1000);
+
+                    if(reply.Status == IPStatus.Success)
+                    {
+                        Console.WriteLine($"Success");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed");
+                    }
+                }
+
+                return "Ok";
+            }
+            catch(Exception ex)
+            {
+                return $"Error : {ex.Message}";
+            }
+        }
+        public static void Main(string[] args)
+        {
+            new Thread(async (x) =>
+            {
+                string url = "www.google.com";
+                int time_out_in_seconds = 3;
+                var str = await Pinger(url, time_out_in_seconds);
+
+                Console.WriteLine($"Hello World! : {str}");
+            }).Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var ping = new Ping();
+        }
+    }
+}
